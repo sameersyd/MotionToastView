fa3936941c93cbcc1b9b13d1c9fd5a6bb4917826
Sameer Nawaz <sameer.nwaz@gmail.com>
2020-08-14 06:10:13 +0530

    Example project with toast customisation

--- Example/MotionToastView/ViewController.swift
+++ Example/MotionToastView/ViewController.swift
@@ -25,13 +25,23 @@ class ViewController: UIViewController {
     }
     
     @IBAction func warningButt(_ sender: UIButton) {
-        MotionToast(message: "You are not in the location. Try again", toastType: .warning, duration: .long, toastStyle: .style_pale, toastGravity: .centre)
+        MotionToast(message: "You are not in the location. Try again", toastType: .warning, duration: .long, toastStyle: .style_pale, toastGravity: .centre, pulseEffect: false)
     }
     
     @IBAction func infoButt(_ sender: UIButton) {
         MotionToast(message: "You have failed to complete the trip", toastType: .info, duration: .long, toastStyle: .style_pale, toastGravity: .top)
     }
-
+    
+    @IBAction func customToastButt(_ sender: UIButton) {
+        MotionToast_Customisation(header: "Custom Toast", message: "It gives you more customisation options.",
+                                  headerColor: UIColor(red: 255.0, green: 255.0, blue: 255.0, alpha: 1.0),
+                                  messageColor: UIColor(red: 239.0, green: 239.0, blue: 239.0, alpha: 0.7),
+                                  primary_color: UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.5),
+                                  secondary_color: UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0),
+                                  icon_image: UIImage(named: "apple")!, duration: .long, toastStyle: .style_pale,
+                                  toastGravity: .bottom, toastCornerRadius: 12, pulseEffect: true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
